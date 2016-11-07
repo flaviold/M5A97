e2b3690cee43536bf1d73d263d54860a34ae6b02
Flavio Duarte <flduarte13@gmail.com>
2016-11-07 16:38:27 -0200

    Better placeholder message for the game command handler

--- game.js
+++ game.js
@@ -5,5 +5,27 @@ var game = module.exports = function (id) {
 };
 
 game.prototype.sendCommand = function (command) {
-  console.log("comando enviado para o jogo " + command.start);
+  var messageArray = [],
+  	  message;
+  command.start && messageArray.push("Start");
+  command.select && messageArray.push("Select");
+
+  command.up && messageArray.push("Up");
+  command.down && messageArray.push("Down");
+  command.left && messageArray.push("Left");
+  command.right && messageArray.push("Right");
+
+  command.a && messageArray.push("A");
+  command.b && messageArray.push("B");
+  command.x && messageArray.push("X");
+  command.y && messageArray.push("Y");
+
+  if (messageArray.length == 0) {
+  	//message = "No button Pressed";
+  	return;
+  } else {
+  	message = "Pressed: " + messageArray.join(", ");
+  }
+
+  console.log(message);
 };
